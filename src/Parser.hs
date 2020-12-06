bcd10e7c4e88898b4612a6f08dead91fe486990d
Gihyeon Sung <gihyeon@gihyeon.xyz>
2020-12-07 00:31:30 +0900

    Create InlineParser

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,6 +3,7 @@
 module Parser where
 
 import Ast
+import Control.Monad.State
 import Data.Text as T
 import Data.Void
 import Text.Megaparsec
@@ -75,3 +76,19 @@ pMathInline = undefined
 
 pSpace :: Parser Inline
 pSpace = Space <$ some space1
+
+type InlineParser = StateT InlineState (Parsec Void Text) Inline
+
+newtype InlineState = InlineState {escapeTokens :: [Text]}
+  deriving (Show, Eq)
+
+pInlinee :: InlineParser
+pInlinee = do
+  ss <- gets escapeTokens
+  case ss of
+    [] -> do
+      cs <- some (alphaNumChar <|> symbolChar)
+      return . Plain $ pack cs
+    s : ss -> do
+      cs <- someTill (alphaNumChar <|> symbolChar) (lookAhead $ string s)
+      modify (\is -> is {escapeTokens = ss}) >> lift (return . Plain $ pack cs)
